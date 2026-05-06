ccbda2798b6300c8dcf83b4cd0c3c3bb4f4b85d0
Santiago-Madera <santiagoyalaxll@gmail.com>
2026-05-06 18:05:51 -0500

    v2: Validación implementada

--- Program.cs
+++ Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+class Programa
+{
+    static int LIMITE_GLOBAL = 80;
+    static void MostrarEncabezado()
+    {
+        Console.WriteLine("==== SISTEMA INDUSTRIAL ====");
+    }
+    static bool ValidarTemperatura(double temp)
+    {
+        if (temp < 0)
+        {
+            Console.WriteLine("Error: valor inválido");
+            return false;
+        }
+        return true;
+    }
+    static String EvaluarTemperatura(double temp)
+    {
+        if (temp > LIMITE_GLOBAL)
+            return "ALERTA";
+        return "NORMAL";
+    }
+    static void Main()
+    {
+        MostrarEncabezado();
+        Console.Write("Ingrese temperatura: ");
+        double temp = double.Parse(Console.ReadLine()!);
+
+        if (ValidarTemperatura(temp))
+        {
+            Console.WriteLine(EvaluarTemperatura(temp));
+        }
+    }
+}
